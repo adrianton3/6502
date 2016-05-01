229ff383688847ce0def4bf656b86a3f74ce6096
Adrian Toncean <adrianton3@gmail.com>
2016-05-01 20:37:17 +0200

    The equalsSubset function can traverse nested structures

--- test/custom-matchers.js
+++ test/custom-matchers.js
@@ -1,6 +1,39 @@
 (function () {
 	'use strict'
 
+	function equalsSubset (actual, expected) {
+		function traverse (actual, expected) {
+			if (actual === expected) {
+				return { matches: true }
+			}
+
+			if (typeof actual === 'object' && actual) {
+				for (let key of Object.keys(expected)) {
+					const { matches, path } = traverse(actual[key], expected[key])
+
+					if (!matches) {
+						return { matches: false, path: [key, ...path] }
+					}
+				}
+			} else {
+				return { matches: false, path: [] }
+			}
+
+			return { matches: true }
+		}
+
+		const { matches, path } = traverse(actual, expected)
+
+		if (!matches) {
+			return {
+				matches,
+				path: path.join('.')
+			}
+		} else {
+			return { matches }
+		}
+	}
+
 	function toEqualSubset (util, customEqualityTesters) {
 		return {
 			compare (actual, expected) {
@@ -11,23 +44,14 @@
 					}
 				}
 
-				let mismatchingKey
-				const matching = Object.keys(expected).every((key) => {
-					const equals = util.equals(actual[key], expected[key])
+				const { matches, path } = equalsSubset(actual, expected)
 
-					if (!equals) {
-						mismatchingKey = key
-					}
-
-					return equals
-				})
-
-				return matching ? {
+				return matches ? {
 					pass: true,
 					message: `Expected the objects to not match`
 				} : {
 					pass: false,
-					message: `Expected the values of the "${mismatchingKey}" property to match`
+					message: `Expected the values of the "${path}" property to match`
 				}
 			}
 		}
